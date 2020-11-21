44df5a725714043fbd39bdf76d74caa466423f20
MR.Shaw <1420218361@qq.com>
2020-11-22 02:46:40 +0800

    优化设计

---
+++ DYLive/DYLive/Classes/Home/Model/GameModel.swift
@@ -0,0 +1,20 @@
+//
+//  GameModel.swift
+//  DYLive
+//
+//  Created by MR.Sahw on 2020/9/26.
+//
+
+import UIKit
+
+class GameModel: BaseGameModel {
+//    @objc var tag_name : String = ""
+//    @objc var pic_url : String = ""
+//
+//    // 自定义构造函数
+//    init(dict : [String : NSObject]) {
+//        super.init()
+//        setValuesForKeys(dict)
+//    }
+//    override func setValue(_ value: Any?, forUndefinedKey key: String) {} // 防止报错
+}
--- DYLive/DYLive/Classes/Tools/Extension/UIDevice-Extension.swift
+++ DYLive/DYLive/Classes/Tools/Extension/UIDevice-Extension.swift
@@ -14,7 +14,7 @@ class UIDevice_Extension: UIDevice {
 extension UIDevice{
     public func isiPhoneXorLater() -> Bool{
         let screenHight = UIScreen.main.nativeBounds.size.height
-        if  screenHight == 2436 || screenHight == 1792 || screenHight == 2688 || screenHight == 1624 {
+        if  screenHight == 2436 || screenHight == 1792 || screenHight == 2688 || screenHight == 1624 || screenHight == 2532 || screenHight == 2778 {
             return true
         }
         return false
